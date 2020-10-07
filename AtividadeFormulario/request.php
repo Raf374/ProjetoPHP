76fe042db0a2ad0a3a09ba9fa56b63f6590c43db
Rafael <rafael.i.porto374@gmail.com>
2020-10-07 18:24:39 -0300

    Terminando atividade

---
+++ AtividadeFormulario/request.php
@@ -0,0 +1,14 @@
+<?php
+    
+    echo "Opinião do site: ".$_REQUEST['nota'];
+    echo "<br>";
+    echo "<br>Sessão gostada: ".$_REQUEST['opcaoa'];
+    echo "<br>Outra: ".$_REQUEST['outra'];
+    echo "<br>Comentarios: ".$_REQUEST['comentario'];
+    echo "<br>Nome: ".$_REQUEST['nome'];
+    echo "<br>E-mail: ".$_REQUEST['email'];
+    echo "<br>Fone: ".$_REQUEST['fone'];
+    echo "<br>FAX: ".$_REQUEST['fax'];
+    echo "<br>Novidades: ".$_REQUEST['novidades'];
+    
+    ?>
