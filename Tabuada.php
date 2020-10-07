9c1beeaef8aa93a60aadb5767d38b9d776aed929
Rafael <rafael.i.porto374@gmail.com>
2020-10-07 16:19:04 -0300

    Primeiro commmit

---
+++ Tabuada.php
@@ -0,0 +1,85 @@
+<?php 
+    echo"<table>";
+        echo"<tr>";
+            //x1
+            echo"<td style='border: 1px solid black'>";
+	            for ($x=0; $x<=10 ; $x++) { 
+                    
+                    echo"1 x ".$x." = ".$x."</br>";
+                }
+            echo"</td>";
+            
+            //2
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 2*$x;
+                    echo"2 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+            
+            //3
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 3*$x;
+                    echo"3 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+            
+            //4
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 4*$x;
+                    echo"4 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+            
+            //5
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 5*$x;
+                    echo"5 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+            
+            //6
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 6*$x;
+                    echo"6 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+            
+            //7
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 7*$x;
+                    echo"7 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+
+            //8
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 8*$x;
+                    echo"8 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+
+            //9
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 9*$x;
+                    echo"9 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+
+            //10
+            echo"<td style='border: 1px solid black'>";
+                for ($x=0; $x<=10; $x++) {
+                    $r= 10*$x;
+                    echo"10 x ".$x." = ".$r."</br>";
+                }
+            echo"</td>";
+        echo"</tr>";
+    echo"<table/>";
+ ?>
